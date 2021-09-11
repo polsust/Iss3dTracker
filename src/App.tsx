459d77b0c3057ed96e4026c7bed91125c1c784a6
Pol Sust <polsust@gmail.com>
2021-09-11 16:10:15 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.sass";
+import { Canvas } from "@react-three/fiber";
+import Earth from "./components/Earth";
+import Iss from "./components/Iss";
+import { OrbitControls } from "@react-three/drei";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Canvas shadows camera={{ position: [-5, 2, 10], fov: 50 }}>
+			<ambientLight intensity={0.5} />
+			{/* <pointLight position={[10, 10, 10]} intensity={1} /> */}
+			<Iss scale={[0.05, 0.05, 0.05]} position={[2, 5, 1]} />
+			<Earth scale={[4.5, 4.5, 4.5]} position={[0, 0, 0]} />
+			<OrbitControls enablePan={false} enableZoom={false} />
+		</Canvas>
+	);
 }
 
 export default App;
