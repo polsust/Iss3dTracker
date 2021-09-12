6a2b1a3737069a07a5ff1d8fe54fe1dc02cfbb5c
Pol Sust <polsust@gmail.com>
2021-09-12 19:11:25 +0200

    earth and Iss working on reload (f5)

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,62 @@
-import React from "react";
 import "./App.sass";
-import { Canvas } from "@react-three/fiber";
+import { Canvas, useFrame } from "@react-three/fiber";
 import Earth from "./components/Earth";
 import Iss from "./components/Iss";
-import { OrbitControls } from "@react-three/drei";
+import { CubeCamera, OrbitControls, Stars } from "@react-three/drei";
+import { Suspense, useRef } from "react";
 
 function App() {
+	const cam = useRef(null);
+
+	setInterval(() => {
+		console.log(cam?.current);
+	}, 5000);
+
 	return (
-		<Canvas shadows camera={{ position: [-5, 2, 10], fov: 50 }}>
-			<ambientLight intensity={0.5} />
-			{/* <pointLight position={[10, 10, 10]} intensity={1} /> */}
-			<Iss scale={[0.05, 0.05, 0.05]} position={[2, 5, 1]} />
-			<Earth scale={[4.5, 4.5, 4.5]} position={[0, 0, 0]} />
-			<OrbitControls enablePan={false} enableZoom={false} />
+		<Canvas /* camera={{ position: [-5, 2, 10], fov: 50 }} */>
+			{/* <ambientLight intensity={0.5} /> */}
+			<mesh ref={cam}>
+				<CubeCamera
+					resolution={256} // Size of the off-buffer (256 by default)
+					frames={Infinity} // How many frames it should render (Indefinitively by default)
+					near={1}
+					far={1000}
+				>
+					{(texture) => (
+						<mesh>
+							<boxGeometry />
+							<meshStandardMaterial envMap={texture} />
+						</mesh>
+					)}
+				</CubeCamera>
+				<pointLight position={[5, 5, 1]} intensity={1} />
+			</mesh>
+
+			<Stars
+				radius={100}
+				depth={50}
+				count={5000}
+				factor={4}
+				saturation={0}
+				fade
+			/>
+
+			<Iss
+				rotation={[0, 0, -0.5]}
+				scale={[0.05, 0.05, 0.05]}
+				position={[3, 3, 0]}
+			/>
+
+			<Earth scale={[4, 4, 4]} />
+
+			<OrbitControls
+				enableDamping={false}
+				rotateSpeed={0.5}
+				maxZoom={0.1}
+				enablePan={false}
+				minZoom={1}
+				zoomSpeed={5}
+			/>
 		</Canvas>
 	);
 }
