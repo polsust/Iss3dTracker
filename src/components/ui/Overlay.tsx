79bb28e6d2142cb0001ce082083b8afb44154efb
Pol Sust <polsust@gmail.com>
2021-11-14 18:03:59 +0100

    Add a  footer with a link to the GitHub repo

--- src/components/ui/Overlay.tsx
+++ src/components/ui/Overlay.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import EmbedVideo from "./EmbedVideo";
 import Metrics from "./Metrics";
+import Sources from "./Sources";
 
 export default function Overlay() {
 	return (
 		<div className="absolute z-50 w-screen h-screen pointer-events-none">
 			{/* <Metrics /> */}
 			<EmbedVideo />
+			<Sources />
 		</div>
 	);
 }
